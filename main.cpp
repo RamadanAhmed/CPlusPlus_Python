5062d24aad7f23c87c6fd56a0e2f20eed5b0602e
Ramadan Ahmed Ramadan <35954554+ramadanahmed@users.noreply.github.com>
2018-03-15 20:00:27 +0200

    Add Pybind11 & Test example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <boost/progress.hpp>
+
+#include <pybind11/embed.h> // everything needed for embedding
+
+namespace py = pybind11;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+    py::module sys = py::module::import("pdb");
+    py::module test_progress = py::module::import("test_progress");
+
+    const unsigned long expected_count = 20;
+    boost::progress_display show_progress(expected_count);
+    for (int i = 0; i != expected_count; ++i) {
+
+        ++show_progress;
+    }
     return 0;
 }
